421a339f46877cafd23b4e69d024a65b04b9dfbb
aritellavsn <153779058+quantnexusai@users.noreply.github.com>
2025-05-03 12:43:25 -0700

    File update

--- setup.py
+++ setup.py
@@ -11,13 +11,6 @@ setup(
         "plotly==5.15.0",
         "numpy==1.23.5",
         "pandas==1.5.3",
-        "requests==2.31.0",
-        "spacy==3.4.4",
-        "thinc==8.1.10",
-        "pydantic==1.10.8",
-        "scispacy==0.5.1"
+        "requests==2.31.0"
     ],
-    dependency_links=[
-        "https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.1/en_core_sci_sm-0.5.1.tar.gz"
-    ]
 )
