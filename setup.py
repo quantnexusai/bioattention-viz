95a7c3a0b52919d3cfb69d447bab5bcbbe04f72a
aritellavsn <153779058+quantnexusai@users.noreply.github.com>
2025-05-03 10:51:24 -0700

    Files updates

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bioattention-viz",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch==2.0.1",
+        "transformers==4.30.2",
+        "streamlit==1.27.2",
+        "plotly==5.15.0",
+        "numpy==1.23.5",
+        "pandas==1.5.3",
+        "requests==2.31.0",
+        "spacy==3.4.4",
+        "thinc==8.1.10",
+        "pydantic==1.10.8",
+        "scispacy==0.5.1"
+    ],
+    dependency_links=[
+        "https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.1/en_core_sci_sm-0.5.1.tar.gz"
+    ]
+)
